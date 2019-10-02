a9c6d55834642b56fa8a2bed480ffe5e2ad085c0
tigercosmos <phy.tiger@gmail.com>
2019-10-02 14:09:47 +0800

    make api

--- index.js
+++ index.js
@@ -67,6 +67,20 @@ if (process.env.USE_CONSOLE === 'true') {
     path: '/telegram'
   });
 
+  server.get('/api/:word', (req, res) => {
+
+    const word = req.params.word;
+
+    const {
+      collectData
+    } = require("./lib/data_process");
+
+    (async () => {
+      const data = await collectData(word)
+      return res.json(data);
+    })();
+  })
+
   const port = process.env.PORT || 8080;
   server.listen(port, () => {
     console.log(`server is listening on ${port} port...`);
